318c1fe9b39255f1a49e8d55f6e82b83e15097f7
bigblackbug <bigblackbugg@gmail.com>
2014-01-24 15:08:45 +0400

    .

---
+++ api/src/main/java/com/icl/integrator/api/ExtendedSourceService.java
@@ -0,0 +1,38 @@
+package com.icl.integrator.api;
+
+import com.icl.integrator.dto.FullServiceDTO;
+import com.icl.integrator.dto.ResponseDTO;
+import com.icl.integrator.dto.ServiceDTO;
+import com.icl.integrator.dto.registration.ActionDescriptor;
+import com.icl.integrator.dto.source.EndpointDescriptor;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: e.shahmaev
+ * Date: 24.01.14
+ * Time: 14:42
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ExtendedSourceService extends SourceService {
+
+    public void handleDeliveryResponse(Map<String, ResponseDTO<UUID>> response);
+
+    public void handleServiceRegistrationResponse(
+            ResponseDTO<Map<String, ResponseDTO<Void>>> response);
+
+    public void handleServiceIsAvailableResponse(ResponseDTO<Boolean> response);
+
+    public void handleGetServiceList(ResponseDTO<List<ServiceDTO>> response);
+
+    public void handleGetSupportedActions(ResponseDTO<List<String>> response);
+
+    public void handleAddAction(ResponseDTO response);
+
+    public <T extends EndpointDescriptor, Y extends ActionDescriptor>
+    void handleGetServiceInfo(ResponseDTO<FullServiceDTO<T, Y>> response);
+
+}
